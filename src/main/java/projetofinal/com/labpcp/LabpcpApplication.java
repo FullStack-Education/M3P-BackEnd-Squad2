b191355504cd665f91a38095f70cfe4e765efee5
gabrielagabriela <gabrielaa.slv@gmail.com>
2024-10-16 19:17:09 -0300

    spring initializr

---
+++ src/main/java/projetofinal/com/labpcp/LabpcpApplication.java
@@ -0,0 +1,13 @@
+package projetofinal.com.labpcp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LabpcpApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LabpcpApplication.class, args);
+	}
+
+}
---
+++ src/test/java/projetofinal/com/labpcp/LabpcpApplicationTests.java
@@ -0,0 +1,13 @@
+package projetofinal.com.labpcp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LabpcpApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
