2bbdfe7d8b65177eca01b1f1f7dfb8cca66f4f31
andrewmatheus <andrewmatheus@gmail.com>
2024-10-27 14:50:29 -0300

    feature: tests materia

--- src/test/java/projetofinal/com/labpcp/service/MateriaServiceTest.java
+++ src/test/java/projetofinal/com/labpcp/service/MateriaServiceTest.java
@@ -1,6 +1,7 @@
 package projetofinal.com.labpcp.service;
 
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
@@ -11,14 +12,18 @@ import projetofinal.com.labpcp.controller.dto.request.MateriaRequest;
 import projetofinal.com.labpcp.controller.dto.response.MateriaResponse;
 import projetofinal.com.labpcp.entity.CursoEntity;
 import projetofinal.com.labpcp.entity.MateriaEntity;
+import projetofinal.com.labpcp.infra.exception.error.NotFoundException;
+import projetofinal.com.labpcp.infra.exception.error.NotFoundExceptionEntity;
 import projetofinal.com.labpcp.repository.CursoRepository;
 import projetofinal.com.labpcp.repository.MateriaRepository;
 import projetofinal.com.labpcp.service.serviceImpl.MateriaServiceImpl;
 
+import java.util.Collections;
 import java.util.List;
 import java.util.Optional;
 
 import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.lenient;
 import static org.mockito.Mockito.when;
 
 @ExtendWith(MockitoExtension.class)
@@ -37,11 +42,16 @@ class MateriaServiceTest {
     @BeforeEach
     void init() {
         curso = new CursoEntity(new CursoRequest("Engenharia de Software", "30"));
+        curso.setId(1L);
         materia = new MateriaEntity("POO", curso);
+        materia.setId(1L);
+        lenient().when(materiaRepository.findById(1L)).thenReturn(Optional.of(materia));
+        lenient().when(materiaRepository.findAll()).thenReturn(Collections.singletonList(materia));
     }
 
     @Test
-    void cadastrarMateria() {
+    @DisplayName("Deve cadastrar materia com sucesso")
+    void deveCadastrarMateriaComSucesso() {
         when(cursoRepository.findById(curso.getId()))
                 .thenReturn(Optional.of(curso));
 
@@ -49,5 +59,71 @@ class MateriaServiceTest {
 
         assertEquals(materia.getNome(), resultado.nome());
         assertEquals(materia.getCurso().getId(), resultado.curso_id());
+    }
+
+    @Test
+    @DisplayName("Deve lançar exceção quando curso não existir ao cadastrar materia")
+    void deveLancarExcecaoQuandoCursoNaoExistir() {
+        when(cursoRepository.findById(curso.getId()))
+                .thenReturn(Optional.empty());
+
+        assertThrows(NotFoundException.class, () -> {
+            materiaService.criar(new MateriaRequest(materia.getNome(), curso.getId()));
+        });
+    }
+
+    @Test
+    @DisplayName("Deve lançar exceção ao buscar materia por id não encontrado")
+    void deveLancarExcecaoQuandoMateriaNaoForEncontradaPeloId() {
+        assertThrows(NotFoundExceptionEntity.class, () -> { materiaService.buscarPorId(0L); });
+    }
+
+    @Test
+    @DisplayName("Deve retornar materia por id")
+    void deveRetornarMateriaPorId() {
+        when(materiaRepository.findById(materia.getId()))
+                .thenReturn(Optional.of(materia));
+
+        when(materiaRepository.findAll())
+                .thenReturn(List.of(materia));
+
+        List<MateriaResponse> listResult = materiaService.buscarTodos();
+
+        System.out.println(listResult);
+
+        MateriaResponse resultado = materiaService.buscarPorId(materia.getId());
+
+        assertEquals(materia.getNome(), resultado.nome());
+    }
+
+    @Test
+    @DisplayName("Deve atualizar materia")
+    void deveAtualizarMateria() {
+        when(materiaRepository.save(materia))
+                .thenReturn(materia);
+
+        materiaService.atualizar(new MateriaRequest("atualizado", curso.getId()), materia.getId());
+
+        assertEquals(materia.getNome(), "atualizado");
+    }
+
+    @Test
+    @DisplayName("Deve deletar materia")
+    void deveDeletarMateria() {
+        materiaService.deletar(materia.getId());
+
+        assertThrows(NotFoundExceptionEntity.class, () -> { materiaService.buscarPorId(materia.getId()); });
+    }
+
+    @Test
+    @DisplayName("Deve retornar lista de materias")
+    void deveRetornarListaDeMaterias() {
+        when(materiaRepository.findAll())
+                .thenReturn(List.of(materia));
+
+        List<MateriaResponse> resultado = materiaService.buscarTodos();
+
+        assertEquals(materia.getNome(), resultado.get(0).nome());
+        assertEquals(materia.getCurso().getId(), resultado.get(0).curso_id());
     }
 }
